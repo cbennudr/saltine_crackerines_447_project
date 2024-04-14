37b9f00e494e311fe4f9dde2a78d2736f96b6124
brinley53 <103296113+brinley53@users.noreply.github.com>
2024-04-14 16:41:58 -0500

    more filtering

--- src/searchpokemon.php
+++ src/searchpokemon.php
@@ -10,8 +10,91 @@ if ($mysqli->connect_error) {
 
 // Send SQL query
 $pokename = $_POST['pokename'];
+$type1 = $_POST['primary_type'];
+$type2 = $_POST['secondary_type'];
 $mysqli->query('DELETE FROM Filtered');
-$query = 'INSERT INTO Filtered SELECT DISTINCT LOWER(Name) FROM Pokemon WHERE Name LIKE "%' . $pokename . '%"';
+$query = 'INSERT INTO Filtered SELECT DISTINCT LOWER(Name) FROM Pokemon';
+$nameq = '';
+$ptypeq = '';
+$type2q = '';
+$_SESSION['form_data'] = $_POST;
+$first = 0;
+$weaknesses = "";
+$wq = '';
+$strengths = '';
+$sq = '';
+
+if ($pokename != '') {
+    $nameq = ' WHERE Name LIKE "%' . $pokename . '%"';
+    $first = 1;
+} 
+if ($type1 != 'primary_type') {
+    if ($first == 1) {
+        $ptypeq = " AND Type1 = '" . $type1 . "'";
+    } else {
+        $first = 1;
+        $ptypeq = " WHERE Type1 = '" . $type1 . "'";
+    }
+}
+if ($type2 != 'secondary_type') {
+    if ($first == 1) {
+        $type2q = " AND Type2 = '" . $type2 . "'";
+    } else {
+        $first = 1;
+        $type2q = " WHERE Type2 = '" . $type2 . "'";
+    }
+}
+if (isset($_POST['weakness'])) {
+    $weaknesses = $_POST['weakness'];
+    // Process the selected weaknesses here
+    if ($first == 1) {
+        $wq = " AND (";
+    } else {
+        $wq = " WHERE (";
+        $first = 1;
+    }
+
+    $count = count($weaknesses);
+    $wrapped_weaknesses = array_map(function($weakness) {
+        return "'" . $weakness . "'";
+    }, $weaknesses);
+
+    $wq .= implode(", ", $wrapped_weaknesses) . ") IN (SELECT Name";
+    for ($i = 0; $i < $count-1; $i++) {
+        $wq .= ", Name";
+    }
+
+    $wq .= " FROM Type_Strength WHERE Strength=Type1 OR Strength=Type2)";
+} 
+if (isset($_POST['strength'])) {
+    $strengths = $_POST['strength'];
+    // Process the selected weaknesses here
+    if ($first == 1) {
+        $sq = " AND (";
+    } else {
+        $sq = " WHERE (";
+        $first = 1;
+    }
+
+    $count = count($strengths);
+    $wrapped_strengths = array_map(function($strength) {
+        return "'" . $strength . "'";
+    }, $strengths);
+
+    $sq .= implode(", ", $wrapped_strengths) . ") IN (SELECT Type_Strength.Strength";
+    for ($i = 0; $i < $count-1; $i++) {
+        $sq .= ", v" . $i . ".Strength";
+    }
+
+    $sq .= " FROM Type_Strength";
+    for ($i = 0; $i < $count-1; $i++) {
+        $sq .= ", Type_Strength v" . $i;
+    }
+    $sq .= " WHERE Type1=Type_Strength.Name OR Type2=Type_Strength.Name)";
+} 
+
+$query .= $nameq . $ptypeq . $type2q . $wq . $sq;
+// echo $query;
 $result = $mysqli->query($query);
 
 if (!$result) {
