f60f6cf674c3bd723c4d3b294bca73457870b944
cbennudr <cbennudriti@hotmail.com>
2024-04-16 20:24:42 -0500

    added team view

---
+++ src/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="http://saltine.wuaze.com/styles/home_styles.css?v=<? echo time(); ?>">
+</head>
+<body>
+
+    
+    <div class="main">
+        <h1 style="margin-left: 2vw"> Pokemon Team Creator </h1>
+        <div class="home_img">
+            <a href="./new_team_creator.php"><img src="images/homepikachu.png"></a>
+        </div>
+    </div>
+
+
+</body>
+</html>
